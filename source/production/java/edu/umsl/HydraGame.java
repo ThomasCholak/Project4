d96414bac0625b812041590765e840f0c399cbdb
tbcwfb <tbcwfb@umsystem.edu>
2022-03-19 00:15:47 -0500

    save 1

--- source/production/java/edu/umsl/HydraGame.java
+++ source/production/java/edu/umsl/HydraGame.java
@@ -11,6 +11,7 @@ package edu.umsl;
 import java.util.Scanner;
 
 public class HydraGame {
+
     public static void main (String[] args) {
         System.out.println(gameDescription()); System.out.println();
 
@@ -18,43 +19,53 @@ public class HydraGame {
         Scanner reader = new Scanner(System.in);
         Hydra myHydra = new Hydra();
 
-        myHydra.hydraHeads(); int inputHeads = reader.nextInt();
-        myHydra.hydraTails(); int inputTails = reader.nextInt();
+        myHydra.hydraHeads(); int inputHeads = reader.nextInt(); //set # of heads
+        myHydra.hydraTails(); int inputTails = reader.nextInt(); //set # of tails
 
-        System.out.println(HydraMonster(inputHeads, inputTails)); System.out.println();
+        System.out.println(HydraMonster(inputHeads, inputTails)); //tell player what monster they've made
+        System.out.println(); String selected = "You have selected: ";
 
-        playerMoves myPlayer = new playerMoves();
+        playerMoves myPlayer = new playerMoves(); //output moves from interface
         myPlayer.MovesAvailable();
         myPlayer.Move1(); myPlayer.Move2(); myPlayer.Move3(); myPlayer.Move4();
         myPlayer.SelectMove();
         int moveSelector = reader.nextInt(); //choose the player's attack(move)
 
-        if (moveSelector < 0 || moveSelector > 4) { System.out.println("please try again."); }
-        if (moveSelector == 1) { myPlayer.Move1(); }
-        if (moveSelector == 2) { myPlayer.Move2(); }
-        if (moveSelector == 3) { myPlayer.Move3(); }
-        if (moveSelector == 4) { myPlayer.Move4(); }
+        if (moveSelector < 0 || moveSelector > 4) { System.out.println("Please try again."); }
+        if (moveSelector == 1) {
+            inputHeads = inputHeads - 1;
+            System.out.print(selected); myPlayer.Move1();
+            System.out.println(HydraMonster(inputHeads, inputTails)); }
+        if (moveSelector == 2) {
+            inputTails = inputTails - 1;
+            System.out.print(selected); myPlayer.Move2();
+            System.out.println(HydraMonster(inputHeads, inputTails)); }
+        if (moveSelector == 3) {
+            inputHeads = inputHeads - 2;
+            System.out.print(selected); myPlayer.Move3();
+            System.out.println(HydraMonster(inputHeads, inputTails)); }
+        if (moveSelector == 4) {
+            inputTails = inputTails - 2;
+            System.out.print(selected); myPlayer.Move4();
+            System.out.println(HydraMonster(inputHeads, inputTails)); }
+    }
 
+    private static String HydraMonster (int heads, int tails) {
+        return "The hydra monster has " + heads + " heads and " + tails + " tails.";
     }
-    public static String HydraMonster (int heads, int tails) {
-        String hydraCreation = "Your hydra monster has " + heads + " heads and " + tails + " tails.";
-        return hydraCreation;
-    }
-    public static String ruleSet () {
+    private static String ruleSet () {
         String Rule0 = "Hydra Game - Here are the handy rules of the game:";
         String Rule1 = "If PyPy cuts off exactly one head, a new head grows immediately.";
         String Rule2 = "If PyPy cuts off exactly one tail, two new tails grow immediately.";
         String Rule3 = "PyPy cuts off exactly two tails, a new head grows immediately.";
         String Rule4 = "If PyPy cuts off exactly two heads, nothing happens.";
-        String Rules = Rule0 + "\n" + Rule1 + "\n" + Rule2 + "\n" + Rule3 + "\n" + Rule4;
-        return Rules;
+        return Rule0 + "\n" + Rule1 + "\n" + Rule2 + "\n" + Rule3 + "\n" + Rule4;
     }
-    public static String gameDescription() {
+    private static String gameDescription() {
         String desc1 = "Princess Perly has been kidnapped by the magical Hydra! The kingdom is in chaos. Now only PyPy — the bravest Knight of the country — can save the day!";
         String desc2 = "Hydra is a powerful magical creature with H heads and T tails. Hydra can breathe fire from each of its heads, and can shoot poison from each of its tails. The only way to";
         String desc3 = "kill Hydra is to cut off all H heads and all T tails. Please note that a Hydra with 0 heads and T > 0 tails is still alive — in which case it is called a ‘Headless Hydra’.";
-        String fullDesc = desc1 + "\n" + desc2 + "\n" + desc3;
-        return fullDesc;
+        return desc1 + "\n" + desc2 + "\n" + desc3;
     }
 }
 
